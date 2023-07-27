6afe67ec72bdbd26c0d0747d6473a9262553b73d
Ashish Rathore <ashrathore98@gmail.com>
2023-07-28 02:11:03 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = @"D:\SimpliLearn Course\Day13\Practice Project\students.txt.txt";
+
+        try
+        {
+            string[] lines = File.ReadAllLines(filePath);
+
+            // Create a list to store student objects
+            var students = new List<Student>();
+
+            foreach (string line in lines.Skip(1)) // Skip the header line
+            {
+                string[] data = line.Split(',');
+                int studentID = int.Parse(data[0]);
+                string name = data[1];
+                string className = data[2];
+                string section = data[3];
+
+                // Create a new Student object and add it to the list
+                var student = new Student(studentID, name, className, section);
+                students.Add(student);
+            }
+
+            // Display the data
+            foreach (var student in students)
+            {
+                Console.WriteLine($"Student ID: {student.StudentID}, Name: {student.Name}, Class: {student.Class}, Section: {student.Section}");
+            }
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("Error: File not found. Make sure the 'students.txt' file exists in the project directory.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Student
+{
+    public int StudentID { get; set; }
+    public string Name { get; set; }
+    public string Class { get; set; }
+    public string Section { get; set; }
+
+    public Student(int studentID, string name, string className, string section)
+    {
+        StudentID = studentID;
+        Name = name;
+        Class = className;
+        Section = section;
+    }
+}
